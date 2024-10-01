3f166f4f0b4f0028d559f1a1ab178000f40e0db8
Chainstrument <minitutoriel@gmail.com>
2024-10-01 16:10:00 +0200

    add component navbar

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import logo from '../../assets/logo.svg'
+
 
 import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-
-    </div>
+    <div className='navbar'>
+        <img src={logo} alt="" />
+        <ul className='nav-menu'>
+            <li>Home</li>
+            <li>About me</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+        </ul>
+        <div className='nav-connect'> Connect wuith me</div>
+    </div> 
   )
 }
 

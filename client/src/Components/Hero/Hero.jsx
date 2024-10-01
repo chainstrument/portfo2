796bf035ba235eb8a23ebefe4a831f44b477fd6f
Chainstrument <minitutoriel@gmail.com>
2024-10-01 16:49:45 +0200

    add part hero

---
+++ client/src/Components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <img src={profile_img} alt="" />
+        <h1><span>I'm Me</span>, frontend developer</h1>
+        <p>I'm a frontend developer, I love to build websites and apps</p>
+        <div className="hero-action">
+            <div className="hero-connect">Connect with me</div>
+            <div className="hero-resume">My resume</div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
